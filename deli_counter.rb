e95b6527f9e3d617591996cd3389febf7604e7e0
Joshua Collins <pc.refuge@gmail.com>
2020-08-01 20:32:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(customers)
+  if customers == 0
+    return "The line is currently empty."
+  else
+    count = 1
+    customers.each do |name|
+      return "The line is currently: #{count}. #{name}"
+      count += 1
+    end
+  end
+end
